025cf65449a56652efb5fef37f0312ec0e64487a
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-04-14 13:42:22 +0300

    add 'relationship' to models

--- models.py
+++ models.py
@@ -1,3 +1,49 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+class Group(Base):
+    __tablename__ = 'Groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+
+class Teacher(Base):
+    __tablename__ = 'Teachers'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(150), nullable=False)
+
+
+class Student(Base):
+    __tablename__ = 'Students'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(150), nullable=False)
+    group_id = Column(Integer, ForeignKey('Groups.id', ondelete='CASCADE'), nullable=False)
+    Groups = relationship("Group", backref='students')
+
+
+class Subject(Base):
+    __tablename__ = 'Subjects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(175), nullable=False)
+    teacher_id = Column(Integer, ForeignKey('Teachers.id', ondelete='CASCADE'), nullable=False)
+    teacher = relationship("Teacher", backref='subjects')
+
+
+
+class Grade(Base):
+    __tablename__ = 'Grades'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('Students.id', ondelete='CASCADE'), nullable=False)
+    subject_id = Column(Integer, ForeignKey('Subjects.id', ondelete='CASCADE'), nullable=False)
+    grade = Column(Integer, nullable=False)
+    grade_date = Column(Date, nullable=False)
+    Students = relationship("Student", backref='grades')
+    Subjects = relationship("Subject", backref='grades')
+
+
+
 """-- Створення таблиці груп
 DROP TABLE IF EXISTS Groups;
 CREATE TABLE Groups (
@@ -43,47 +89,3 @@ CREATE TABLE Grades (
     grade INTEGER CHECK (grade >= 0 and grade <= 100),
     grade_date DATE NOT NULL
 );"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
-from sqlalchemy.orm import relationship, declarative_base
-
-Base = declarative_base()
-
-class Group(Base):
-    __tablename__ = 'Groups'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-
-
-class Teacher(Base):
-    __tablename__ = 'Teachers'
-    id = Column(Integer, primary_key=True)
-    full_name = Column(String(150), nullable=False)
-
-
-class Student(Base):
-    __tablename__ = 'Students'
-    id = Column(Integer, primary_key=True)
-    full_name = Column(String(150), nullable=False)
-    group_id = Column(Integer, ForeignKey('Groups.id', ondelete='CASCADE'), nullable=False)
-    Groups = relationship("Group", backref='students')
-
-
-class Subject(Base):
-    __tablename__ = 'Subjects'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(175), nullable=False)
-    teacher_id = Column(Integer, ForeignKey('Teachers.id', ondelete='CASCADE'), nullable=False)
-    teacher = relationship("Teacher", backref='subjects')
-
-
-
-class Grade(Base):
-    __tablename__ = 'Grades'
-    id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('Students.id', ondelete='CASCADE'), nullable=False)
-    subject_id = Column(Integer, ForeignKey('Subjects.id', ondelete='CASCADE'), nullable=False)
-    grade = Column(Integer, nullable=False)
-    grade_date = Column(Date, nullable=False)
-    Students = relationship("Student", backref='grades')
-    Subjects = relationship("Subject", backref='grades')
