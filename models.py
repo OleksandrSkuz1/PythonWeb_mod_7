f0e36bf62e47f1c9e34f472e9fc7e43ed24500ea
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-04-14 11:34:51 +0300

    add 'relationship' to models

--- models.py
+++ models.py
@@ -45,7 +45,7 @@ CREATE TABLE Grades (
 );"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
-from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
@@ -55,32 +55,35 @@ class Group(Base):
     name = Column(String(50), nullable=False)
 
 
-class Student(Base):
-    __tablename__ = 'Students'
-    id = Column(Integer, primary_key=True)
-    full_name = Column(String(150), nullable=False)
-    group_id = Column(Integer, ForeignKey('Groups.id'), nullable=False)
-
-
 class Teacher(Base):
     __tablename__ = 'Teachers'
     id = Column(Integer, primary_key=True)
     full_name = Column(String(150), nullable=False)
 
 
+class Student(Base):
+    __tablename__ = 'Students'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(150), nullable=False)
+    group_id = Column(Integer, ForeignKey('Groups.id', ondelete='CASCADE'), nullable=False)
+    Groups = relationship("Group", backref='students')
+
+
 class Subject(Base):
     __tablename__ = 'Subjects'
     id = Column(Integer, primary_key=True)
     name = Column(String(175), nullable=False)
-    teacher_id = Column(Integer, ForeignKey('Teachers.id'), nullable=False)
+    teacher_id = Column(Integer, ForeignKey('Teachers.id', ondelete='CASCADE'), nullable=False)
+    teacher = relationship("Teacher", backref='subjects')
+
 
 
 class Grade(Base):
     __tablename__ = 'Grades'
     id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('Students.id'), nullable=False)
-    subject_id = Column(Integer, ForeignKey('Subjects.id'), nullable=False)
+    student_id = Column(Integer, ForeignKey('Students.id', ondelete='CASCADE'), nullable=False)
+    subject_id = Column(Integer, ForeignKey('Subjects.id', ondelete='CASCADE'), nullable=False)
     grade = Column(Integer, nullable=False)
     grade_date = Column(Date, nullable=False)
-
-
+    Students = relationship("Student", backref='grades')
+    Subjects = relationship("Subject", backref='grades')
